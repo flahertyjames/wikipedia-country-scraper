1038e6841b9c5b1802bbf85dae6df44fb3d53df2
James Flaherty <james.flaherty@activenetwork.com>
2013-04-07 21:15:52 -0400

    added base subdivision tests; nuspec document

---
+++ test/Flaherty.WikipediaCountryScraper.Tests/AdministrativeSubdivisionScraperTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Flaherty.WikipediaCountryScraper.Tests
+{
+    [TestClass]
+    public class AdministrativeSubdivisionScraperTest
+    {
+        private static IEnumerable<AdministrativeSubdivision> _usSubdivisions;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var scraper = new AdministrativeSubdivisionScraper("US");
+            _usSubdivisions = scraper.Scrape();
+        }
+
+        [TestMethod]
+        public void AdministrativeSubdivisionScraper_Scrape_ReturnsData()
+        {
+            Assert.IsTrue(_usSubdivisions.Any());
+        }
+
+        [TestMethod]
+        public void AdministrativeSubdivisionScraper_Scrape_ContiansValidData()
+        {
+            var ny = _usSubdivisions.FirstOrDefault(x => x.IsoCode == "US-NY");
+            Assert.IsNotNull(ny);
+            Assert.AreEqual("New York", ny.Name);
+        }
+    }
+}
